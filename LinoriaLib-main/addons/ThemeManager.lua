39aee30173e29cc7d8911d2901f15929eed5a2ea
Seraph-Trash <143275451+Seraph-Trash@users.noreply.github.com>
2023-08-27 15:01:21 +0700

    Update ThemeManager.lua

--- LinoriaLib-main/addons/ThemeManager.lua
+++ LinoriaLib-main/addons/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Seraph'] 	 	    = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"011012","AccentColor":"00e8ff","BackgroundColor":"021114","OutlineColor":"07282a"}') },
+		['Seraph Hub Premium Script'] 	 	    = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"04131d","AccentColor":"1ba2dc","BackgroundColor":"030d0f","OutlineColor":"082021"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
